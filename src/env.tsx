c7792fda6514e8ddbc08885db86dc2e324941b73
rollno748 <rollno748@gmail.com>
2023-07-03 17:24:20 +0100

    fixed env variables

--- src/env.tsx
+++ src/env.tsx
@@ -1,4 +1,4 @@
 export const INFLUX_URL = 'https://europe-west1-1.gcp.cloud2.influxdata.com';
-export const INFLUX_TOKEN = 'Felidq3TRB3S0IGObZmTt0lAJcxMcIIhJtnhwYwsiB9KYMGNtk7LQlEcYfa0wQUoB3YAaJI4AtF-3n1OXOtYvg==';
-export const INFLUX_ORG = 'perfwise';
-export const INFLUX_BUCKET = 'jmeter';
+export const INFLUX_TOKEN = '{{INFLUX_TOKEN}}';
+export const INFLUX_ORG = '{{INFLUX_ORG}}';
+export const INFLUX_BUCKET = '{{INFLUX_BUCKET}}';
